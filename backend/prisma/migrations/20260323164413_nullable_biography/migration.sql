c2390c04d4cc465b7534aebd9e846ddf65a8eb0f
Tomas Bregoli <tomasbrg42@gmail.com>
2026-03-23 13:54:26 -0300

    added prisma service some methods

---
+++ backend/prisma/migrations/20260323164413_nullable_biography/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "biography" DROP NOT NULL;
--- backend/src/services/register.service.ts
+++ backend/src/services/register.service.ts
@@ -1,7 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from '../dto/register.dto';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class RegisterService {
-  createUser(registerDto: RegisterDto) {}
+  constructor(private prismaService: PrismaService) {}
+  async createUser(registerDto: RegisterDto): Promise<void> {
+    await this.prismaService.user.create({
+      data: {
+        username: registerDto.username,
+        email: registerDto.email,
+        password: registerDto.password, //maybe hash it in the front or when we receive it
+        biography: null,
+      },
+    });
+  }
 }
