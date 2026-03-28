5860802492f28f1d767501a0e44d170e3e7a7ba0
luciabetelu <lubetelu@gmail.com>
2026-03-28 12:01:23 -0300

    validation min two sports selected

--- backend/src/preferences/dto/preferences.dto.ts
+++ backend/src/preferences/dto/preferences.dto.ts
@@ -1,9 +1,10 @@
-import { IsDefined } from 'class-validator';
+import { ArrayMinSize, IsDefined } from 'class-validator';
 
 export class PreferencesDto {
   @IsDefined()
   username: string;
 
+  @ArrayMinSize(2)
   sports: string[];
   level: string;
 }
