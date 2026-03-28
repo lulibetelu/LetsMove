ab9f9613af504afe6f356b604ac0b0a3096f2778
Tomas Bregoli <tomasbrg42@gmail.com>
2026-03-27 21:27:09 -0300

    refactored login because of one query

--- backend/src/login/login.service.ts
+++ backend/src/login/login.service.ts
@@ -1,17 +1,17 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
+import { UserRepositoryService } from '../repository/user.repository.service';
 
 @Injectable()
 export class LoginService {
   constructor(
     private prismaService: PrismaService,
     private jwtService: JwtService,
+    private userRepositoryService: UserRepositoryService,
   ) {}
   async login(email: string, pass: string) {
-    const user = await this.prismaService.user.findUnique({
-      where: { email: email },
-    });
+    const user = await this.userRepositoryService.findByEmail(email);
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
