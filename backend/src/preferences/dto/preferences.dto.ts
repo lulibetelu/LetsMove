db45dddaa5b20909afa700199851e5fd6423bd6e
luciabetelu <lubetelu@gmail.com>
2026-03-28 11:47:26 -0300

    agregue validacion basica para los dtos

--- backend/src/preferences/dto/preferences.dto.ts
+++ backend/src/preferences/dto/preferences.dto.ts
@@ -1,5 +1,9 @@
+import { IsDefined } from 'class-validator';
+
 export class PreferencesDto {
+  @IsDefined()
   username: string;
+
   sports: string[];
   level: string;
 }
--- backend/src/register/dto/register.dto.ts
+++ backend/src/register/dto/register.dto.ts
@@ -1,5 +1,12 @@
+import { IsDefined, IsEmail, IsNotEmpty } from 'class-validator';
+
 export class RegisterDto {
+  @IsDefined()
   username: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
