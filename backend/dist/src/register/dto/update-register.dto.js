3807464cba4a660f72f970e04d94f28859101f9d
Tomas Bregoli <tomasbrg42@gmail.com>
2026-03-28 20:33:48 -0300

    fix: reconfiguring the monorepo

--- backend/dist/src/register/dto/update-register.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateRegisterDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const register_dto_1 = require("./register.dto");
-class UpdateRegisterDto extends (0, mapped_types_1.PartialType)(register_dto_1.RegisterDto) {
-}
-exports.UpdateRegisterDto = UpdateRegisterDto;
-//# sourceMappingURL=update-register.dto.js.map
