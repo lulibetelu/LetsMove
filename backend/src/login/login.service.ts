6628e1b769fdb48acb3bb1524b1fa86695670c34
luciabetelu <lubetelu@gmail.com>
2026-03-28 11:31:07 -0300

    agrego comentarios

--- backend/src/login/login.service.ts
+++ backend/src/login/login.service.ts
@@ -12,11 +12,16 @@ export class LoginService {
     const user = await this.prismaService.user.findUnique({
       where: { email: email },
     });
+    //falta hashear
     if (user?.password !== pass) {
       throw new UnauthorizedException();
     }
+    // payload ~= token, sub = subject
+    // se crea el token con el id y el email del user
     const payload = { sub: user.id, email: user.email };
 
+    // payload + secret key = token
+    // luego el cliente recibe el token, lo guarda y lo manda al header
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
