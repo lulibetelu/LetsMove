7c377cfe8a3eb6f48b915e5d64a44793b149c244
luciabetelu <lubetelu@gmail.com>
2026-04-04 16:18:22 -0300

    agrego token auth en api posts front

--- frontend/src/api/post.ts
+++ frontend/src/api/post.ts
@@ -2,9 +2,13 @@ import type {Post} from "../types/postTypes.ts";
 const url = import.meta.env.VITE_API_URL;
 
 export async function create(postInfo: Post){
+    const token = localStorage.getItem('token');
     const response = await fetch(url + 'posts' , {
         method: 'Post',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
         body: JSON.stringify(postInfo)
     });
     if (!response.ok) throw new Error(`Failed to create post: ${response.status}`);
@@ -12,27 +16,39 @@ export async function create(postInfo: Post){
 }
 
 export async function findAll(){
+    const token = localStorage.getItem('token');
     const response = await fetch(url + 'posts' , {
         method: 'Get',
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
     });
     if (!response.ok) throw new Error(`Failed to load posts: ${response.status}`);
     return response.json();
 }
 
 export async function findOne(postId: number){
+    const token = localStorage.getItem('token');
     const response = await fetch(url + 'posts/' + postId , {
         method: 'Get',
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
     });
     if (!response.ok) throw new Error(`Failed to load post with id ${postId}: ${response.status}`);
     return response.json();
 }
 
 export async function remove(postId: number){
+    const token = localStorage.getItem('token');
     const response = await fetch(url + 'posts' + postId , {
         method: 'Delete',
-        headers: { 'Content-Type': 'application/json' }
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }
     });
     if (!response.ok) throw new Error(`Failed to delete post with id ${postId}: ${response.status}`);
     return response.json();
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+import {useState} from "react";
+
+export default function HomePage() {
+    const [posts, addPosts] = useState([]);
+}
