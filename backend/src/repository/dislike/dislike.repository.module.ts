ffef24007814f0390579bc99c765e263bd7d43f2
Tomas Bregoli <tomasbrg42@gmail.com>
2026-04-07 10:58:11 -0300

    defined dislike repository module

--- backend/src/repository/dislike/dislike.repository.module.ts
+++ backend/src/repository/dislike/dislike.repository.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { DislikeRepositoryService } from './dislike.repository.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
-  imports : [PrismaModule],
-  providers : []
-  }
-)
+  imports: [PrismaModule],
+  providers: [DislikeRepositoryService],
+  exports: [DislikeRepositoryService],
+})
+export class DislikeRepositoryModule {}
