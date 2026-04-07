0c78b03ec55478b13cf880b9f16425592c733502
Tomas Bregoli <tomasbrg42@gmail.com>
2026-04-07 10:59:37 -0300

    added imports on dislike module

--- backend/src/dislike/dislike.module.ts
+++ backend/src/dislike/dislike.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DislikeService } from './dislike.service';
 import { DislikeController } from './dislike.controller';
+import { DislikeRepositoryModule } from '../repository/dislike/dislike.repository.module';
 
 @Module({
+  imports: [DislikeRepositoryModule],
   controllers: [DislikeController],
   providers: [DislikeService],
 })
