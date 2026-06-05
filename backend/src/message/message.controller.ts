5cc32898acd4e2e74c213c599627c37b73c462ac
Tomas Bregoli <tomasbregoli@Tomas-Bregolis-MacBook-Pro.local>
2026-06-05 10:57:57 -0300

    added guards to the endpoints

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -5,18 +5,23 @@ import {
   Body,
   ParseIntPipe,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message.dto';
+import { AuthGuard } from '../authentication/auth.guard';
 
 @Controller('message')
 export class MessageController {
   constructor(private readonly messageService: MessageService) {}
 
+  @UseGuards(AuthGuard)
   @Get()
   findAll(@Query('groupId', ParseIntPipe) groupId: number) {
     return this.messageService.findAll(groupId);
   }
+
+  @UseGuards(AuthGuard)
   @Post()
   create(@Body() createMessageDto: CreateMessageDto) {
     return this.messageService.create(createMessageDto);
