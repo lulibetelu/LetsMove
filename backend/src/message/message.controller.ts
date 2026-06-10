9061b4f61d2ee09c1e88bd740bacd6ff99c5c475
Tomas Bregoli <tomasbregoli@Tomas-Bregolis-MacBook-Pro.local>
2026-06-10 10:06:11 -0300

    added a isMember verification in message controller

--- backend/src/message/message.controller.ts
+++ backend/src/message/message.controller.ts
@@ -6,18 +6,33 @@ import {
   ParseIntPipe,
   Query,
   UseGuards,
+  Req,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { AuthGuard } from '../authentication/auth.guard';
+import type { Request } from 'express';
+import { GroupService } from '../group/group.service';
 
 @Controller('message')
 export class MessageController {
-  constructor(private readonly messageService: MessageService) {}
+  constructor(
+    private readonly messageService: MessageService,
+    private readonly groupService: GroupService,
+  ) {}
 
   @UseGuards(AuthGuard)
   @Get()
-  findAll(@Query('groupId', ParseIntPipe) groupId: number) {
+  async findAll(
+    @Req() req: Request,
+    @Query('groupId', ParseIntPipe) groupId: number,
+  ) {
+    const userId: number = req.user.sub;
+    const isMember: boolean = await this.groupService.isMember(userId, groupId);
+
+    if (!isMember)
+      throw new UnauthorizedException('Requester must be group member');
     return this.messageService.findAll(groupId);
   }
 
